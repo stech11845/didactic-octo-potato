144cc83df5e2ab831806ad2e6479a1a6f2649122
stech11 (SoftOrange Tech) <108250871+stech11845@users.noreply.github.com>
2022-07-15 15:55:11 -0600

    Initial commit

---
+++ rollup.config.js
@@ -0,0 +1,20 @@
+import { createPlugins } from "rollup-plugin-atomic"
+
+const plugins = createPlugins(["js", "babel"])
+
+const RollupConfig = [
+  {
+    input: "src/main.js",
+    output: [
+      {
+        dir: "dist",
+        format: "cjs",
+        sourcemap: true,
+      },
+    ],
+    // loaded externally
+    external: ["atom"],
+    plugins,
+  },
+]
+export default RollupConfig
